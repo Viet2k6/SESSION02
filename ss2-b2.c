ebda922d1118b676fb36ea5c3722932094774e9f
Đăng Việt <trandangviet00@gmail.com>
2024-11-20 20:53:05 +0700

    Add files via upload

---
+++ ss2-b2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+   int firstNumber = 10;
+   
+   int secondNumber = 8;
+   
+   int total = firstNumber + secondNumber;
+   
+   
+   printf("Tong la %d\n",total);
+   
+   
+   
+   
+   return 0;
+}
+   
