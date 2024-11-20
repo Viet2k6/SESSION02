267cf53702353fc1bb38355017454ff92e2796c3
Đăng Việt <trandangviet00@gmail.com>
2024-11-20 21:04:53 +0700

    Add files via upload

---
+++ ss2-b4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int canh = 7;
+    printf("Canh la %d \n",canh);
+    
+    int chu_vi = 4 * canh;
+    
+    printf("Chu vi la %d\n",chu_vi);
+    
+    int dien_tich = canh * canh;
+    printf("Dien tich la %d",dien_tich);
+   return 0;
+}
