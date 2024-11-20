d508e9bad372f6c129bc3d614d1a3f1fb8ac78b4
Đăng Việt <trandangviet00@gmail.com>
2024-11-20 20:42:35 +0700

    Add files via upload

---
+++ ss2-b1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+  int name;
+  //Kieu du lieu so nguyen
+	
+  float height;
+  //Kieu du lieu so thuc
+	
+  double pi;
+  //Kieu du lieu so thuc nhung do chinh xac cao hon
+	
+  char k;
+  //Kieu du lieu ky tu
+	
+  return 0;
+	
+}
+	
